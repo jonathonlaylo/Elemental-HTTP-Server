444ecc8b0919c06a7ecbbb87bc43b8689daf81dd
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-31 17:02:42 -1000

    created resourceMapping to go through routing table

--- server.js
+++ server.js
@@ -3,8 +3,63 @@ const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer( (req, res) => {
+const fileNotFoundErrorHandler = (res) => {
+  res.statusCode = 500;
+  res.end('Server is broken');
+};
 
+const sendContent = (res, content) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write(content);
+  res.end();
+};
+
+const resourceMapping = {
+  '/helium.html' : './public/helium.html',
+  '/hydrogen.html' : './public/hydrogen.html',
+  '/index.html' : './public/index.html',
+  '/styles.css' : "./public/css/styles.css"
+};
+
+const server = http.createServer( (req, res) => {
+  if(req.url === '/'){
+    req.url = '/index.html';
+  }
+
+  console.log("req.url", req.url);
+  console.log("req.method", req.method);
+  console.log("req.headers", req.headers);
+
+  let reqBody = '';
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => {
+    reqBody += chunk;
+  });
+  req.on('end', () => {
+    // reqBody is complete, handle request!
+    console.log(reqBody);
+
+    fs.readFile(resourceMapping[req.url] || '', (err, content) => {
+      if(err){
+        res.statusCode = 404;
+        sendContent(res, 'Resource not found');
+        return;
+      }
+      sendContent(res, content);
+    });
+  });
+
+  // fs.readFile('./public/helium.html', (err, content) =>{
+  //   if(err){
+  //     res.statusCode = 500;
+  //     res.write("Server fault occurred");
+  //     res.end();
+  //     return;
+  //   }
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   res.write(content);
+  //   res.end();
+  // });
 
 });
 
